bc3716ce43193f65010264e21ba1a6768f32a651
Dagm beke <dagmawitbekele05gmail.com>
2025-09-02 22:41:40 +0900

    push1

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   plugins: [react(), tailwindcss(),],
+  base: "/",
   server: {
     port: 5173, // Explicitly set the port
     proxy: {
